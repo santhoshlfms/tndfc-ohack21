f4ca1e88d0c822e1776bf7d6f70391e4614014fe
Santhosh Nelson <santhoshlfms@gmail.com>
2022-04-20 15:56:25 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import AuthProvider from "../providers/AuthProvider";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <AuthProvider><Component {...pageProps} /><AuthProvider>
+  return <AuthProvider><Component {...pageProps} /></AuthProvider>
 }
 
 export default MyApp
