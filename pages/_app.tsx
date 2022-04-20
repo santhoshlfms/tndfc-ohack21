3fd81e48d30ff6516c72cbc877de55797320c902
Santhosh Nelson <santhoshlfms@gmail.com>
2022-04-20 15:34:38 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import AuthProvider from "../providers/AuthProvider";
 
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (<AuthProvider><Component {...pageProps} /></AuthProvider>)
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+   const getLayout =
+    (Component as MyNextComponentType).getLayout ||
+    ((page: JSX.Element): JSX.Element => <AppLayout children={page} />);
+
+  return ( <AuthProvider>{getLayout(<Component {...pageProps} />)}</AuthProvider>)
 }
 export default MyApp
